60ebb4d4860128905c699913736ee6c35b29c0df
Mikhail Nikorkin <nikorkin1990@mail.ru>
2022-11-29 16:27:05 +0300

    create task

---
+++ task/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] arrayLine = { "Hello", "2", "world", ":-)" };
+string[] arrayNumbersLine = new string[GetStrings(arrayLine)];
+FillArray(arrayLine, arrayNumbersLine);
+System.Console.WriteLine("'{0}'", string.Join("', '", arrayNumbersLine));
+
+int GetStrings(string[] arrayString)
+{
+    int arrayLength = 0;
+    for (int i = 0; i < arrayString.Length; i++)
+        if (arrayString[i].Length <= 3) arrayLength++;
+    return arrayLength;
+}
+
+void FillArray(string[] arrayLine, string[] arrayNumbersLine)
+{
+    int j = 0;
+
+    for (int i = 0; i < arrayLine.Length; i++)
+    {
+        if (arrayLine[i].Length <= 3)
+        {
+            arrayNumbersLine[j] = arrayLine[i];
+            j++;
+        }
+    }
+}
